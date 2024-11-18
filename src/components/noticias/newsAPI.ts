5cc8a4d4462f1717d23ca20bb9a0fcb2dcd2a5af
bonomo06 <pedrohbnm@gmail.com>
2024-11-18 17:52:53 -0300

    arrumando erro na API

--- src/components/noticias/newsAPI.ts
+++ src/components/noticias/newsAPI.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-const API_KEY = "cd4468b2d7c14c9993c402fa4ed51e80";
-const BASE_URL = "https://newsapi.org/v2/everything?q=dengue&apiKey=cd4468b2d7c14c9993c402fa4ed51e80";
+const BASE_URL = "https://newsapi.org/v2/everything";
 
 export const getNewsAboutDengue = async () => {
   try {
     const response = await axios.get(BASE_URL, {
       params: {
         q: "dengue", // A palavra-chave para buscar notícias sobre dengue
-        apiKey: API_KEY,
+        apiKey: "cd4468b2d7c14c9993c402fa4ed51e80",
         language: "pt", // Notícias em português
         sortBy: "publishedAt", // Ordena as notícias pela data de publicação
         pageSize: 6, // Limita o número de resultados
