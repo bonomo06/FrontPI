aef48fc9f936f8f9de693d1a11a0cb270b6ca736
bonomo06 <pedrohbnm@gmail.com>
2024-11-18 16:42:17 -0300

    arrumando erro vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  experimental: {
+    appDir: false, // Desative o modo app caso esteja ativado
+  },
 };
-
-export default nextConfig;
+module.exports = nextConfig;
